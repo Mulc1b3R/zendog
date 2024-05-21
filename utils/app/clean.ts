7cf7020f2784853ba6a1561ea9945e3b1005bed3
psico-mulciber <158625834+Mulc1b3R@users.noreply.github.com>
2024-05-21 17:11:52 +0100

    xxx

--- utils/app/clean.ts
+++ utils/app/clean.ts
@@ -3,14 +3,11 @@ import { OpenAIModelID, OpenAIModels } from '@/types/openai';
 import { DEFAULT_SYSTEM_PROMPT } from './const';
 
 export const cleanSelectedConversation = (conversation: Conversation) => {
-  // added model for each conversation (3/20/23)
-  // added system prompt for each conversation (3/21/23)
-  // added folders (3/23/23)
-  // added prompts (3/26/23)
+  
 
   let updatedConversation = conversation;
 
-  // check for model on each conversation
+  
   if (!updatedConversation.model) {
     updatedConversation = {
       ...updatedConversation,
